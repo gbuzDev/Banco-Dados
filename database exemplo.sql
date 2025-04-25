8622f33a55dcd04075b60098ae5a8b94f241b72e
Gabriel Silva Gomes <gabriel.silvagomes2006@gmail.com>
2025-04-24 21:41:03 -0300

    INCOMPLETO
    24/04/25
    exemplo como criar um banco de dados fisico
    *INCOMPLETO

---
+++ database exemplo.sql
@@ -0,0 +1,18 @@
+drop database if exists curso_mysql_senac_1_semestre_2025;
+create database curso_mysql_senac_1_semestre_2025;
+use curso_mysql_senac_1_semestre_2025;
+
+show databases;
+
+drop table if exists func;
+#criação de tabela - Primeiro lado 1 da cardinalidade
+#Só tenho chaves primarias
+create table func (
+idFunc int unsigned not null auto_increment PRIMARY KEY,
+nome varchar(200) not null,
+salario double not null default '0',
+deprt varchar(100) not null,
+data_admisao datetime);
+
+select * from func
+select idFunc
